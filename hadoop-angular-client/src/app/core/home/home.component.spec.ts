a30eba5bd32ebbb4e920b1c05871c8ba51c98ed5
kanzt <49674128+kanzt@users.noreply.github.com>
2019-10-03 16:00:43 +0700

    Add files via upload

--- hadoop-angular-client/src/app/core/home/home.component.spec.ts
+++ hadoop-angular-client/src/app/core/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HomeService} from './home.service';
+import {TableModule} from 'primeng/table';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +12,9 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ReactiveFormsModule, TableModule, HttpClientModule],
+      providers: [HomeService]
     })
     .compileComponents();
   }));
--- hadoop-angular-client/src/app/core/home/home.service.spec.ts
+++ hadoop-angular-client/src/app/core/home/home.service.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { HomeService } from './home.service';
+import {HomeService} from './home.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('HomeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [HomeService],
+    imports: [HttpClientModule]
+  }));
 
-  it('should be created', () => {
-    const service: HomeService = TestBed.get(HomeService);
+  // it('should be created', () => {
+  //   const service: HomeService = TestBed.get(HomeService);
+  //   expect(service).toBeTruthy();
+  // });
+  it('should be created', inject([HomeService], (service: HomeService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
